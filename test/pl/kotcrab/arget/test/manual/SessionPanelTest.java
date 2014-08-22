997962c065b640995c9fbf0ec887fde6ab5b21ce
Paweł Pastuszak <kotcrab@gmail.com>
2014-08-22 15:57:19 +0200

    Applied license to tests

--- test/pl/kotcrab/arget/test/manual/SessionPanelTest.java
+++ test/pl/kotcrab/arget/test/manual/SessionPanelTest.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+    Copyright 2014 Pawel Pastuszak
+ 
+    This file is part of Arget.
+
+    Arget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Arget is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Arget.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 
 package pl.kotcrab.arget.test.manual;
 
@@ -63,7 +81,7 @@ public class SessionPanelTest extends JFrame {
 				Msg.RIGHT,
 				"Linki: http://mashable.com/2014/08/14/watch-surgery-on-the-oculus-rift-but-maybe-do-it-after-lunch/?utm_cid=mash-com-G+-main-link"));
 
-		panel.addMessage(new TextMessage(Msg.LEFT, "Obrazki w okienku rozmowy i przesy³anie plików"));
+		panel.addMessage(new TextMessage(Msg.LEFT, "Obrazki w okienku rozmowy i przesyï¿½anie plikï¿½w"));
 // panel.addMessage(new ImageMessage(Msg.LEFT, ImageUitls.read(new File("avatar.jpg")), ""));
 
 		// panel.addMessage(new FileTransferMessage(new SendFileTask(null, new File("test.txt"), false)));
