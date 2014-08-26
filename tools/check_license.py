54dbef2daed14dfec4b9e96b9708bd7da1b6ec9c
Paweł Pastuszak <kotcrab@gmail.com>
2014-08-26 14:41:26 +0200

    Added missing license headers

--- tools/check_license.py
+++ tools/check_license.py
@@ -1,7 +1,7 @@
 import fnmatch
 import os
 
-#config
+# config
 main_dir = os.path.dirname(os.getcwd())
 src_dir = os.path.join(main_dir, 'src', 'pl', 'kotcrab', 'arget')
 header = os.path.join(main_dir, 'src', 'data', 'license_header.txt')
@@ -17,12 +17,16 @@ def contains(small, big):
     return True
 
 def process_file(file_path):
+    global missing
     file = open(file_path)
     source = file.readlines()
     file.close()
     
     if not contains(header_lines, source):
+        missing += 1
         print 'WARNING: Missing header: ' + file_path
+
+missing = 0
 
 f = open(header)
 header_lines = f.readlines()
@@ -31,3 +35,5 @@ f.close()
 for root, dirnames, filenames in os.walk(main_dir):
   for filename in fnmatch.filter(filenames, '*.java'):
       process_file(os.path.join(root, filename))
+      
+print 'Done, missing: ' + str(missing)
