a3926eb24a9187d21b61088b3c3b8c5555d8b5c3
Paweł Pastuszak <kotcrab@gmail.com>
2014-08-27 21:07:12 +0200

    New About dialog

--- src/pl/kotcrab/arget/gui/dialog/AboutDialog.java
+++ src/pl/kotcrab/arget/gui/dialog/AboutDialog.java
@@ -20,64 +20,72 @@
 package pl.kotcrab.arget.gui.dialog;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 import net.miginfocom.swing.MigLayout;
-
-import org.apache.commons.io.IOUtils;
-
 import pl.kotcrab.arget.App;
 import pl.kotcrab.arget.gui.components.ESCClosableDialog;
+import pl.kotcrab.arget.util.StringUtils;
 
-//TODO add gpl license, add more info
 public class AboutDialog extends ESCClosableDialog {
+	private AboutDialog instance;
 
 	public AboutDialog (JFrame owner) {
 		super(owner, true);
-		setSize(450, 300);
+		instance = this;
+
+		setResizable(false);
+		setSize(423, 150);
 		setTitle(App.APP_NAME + " - About");
-		setMinimumSize(new Dimension(450, 300));
 		setPositionToCenter(owner);
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		getContentPane().setLayout(new BorderLayout());
 
-		JPanel mainPanel = new JPanel();
+		JPanel mainPanel = new JPanel(new MigLayout("", "[grow][][]", "[][][grow][]"));
 		getContentPane().add(mainPanel, BorderLayout.CENTER);
 
-		mainPanel.setLayout(new MigLayout("", "[grow]", "[][][grow][]"));
-
-		JTextArea licenseTextArea = new JTextArea();
-
-		try { // Comment this if you want to use gui designer, otherwise you will get java heap space exception
-			licenseTextArea.setText(IOUtils.toString(App.getResourceAsStream("/data/3rdlicense.txt")));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		licenseTextArea.setCaretPosition(0);
+		mainPanel.add(new JLabel("Arget - open source chat program"), "cell 0 0 2 1");
+		mainPanel.add(new JLabel(new ImageIcon(AboutDialog.class.getResource("/data/icon/icon_med.png"))),
+			"cell 2 0 1 2,alignx right");
+		mainPanel.add(new JLabel("Created by Paweł Pastszuak, licensed under GPLv3 license."), "cell 0 1 2 1,aligny top");
+		mainPanel.add(new JLabel("Version: " + App.APP_NAME + " " + App.APP_VERSION), "flowx,cell 0 3,alignx left");
 
 		JButton okButton = new JButton("OK");
+		JButton viewLicenseButton = new JButton("View license");
+		JButton view3rdLicenseButton = new JButton("View 3rd licenses");
+
+		mainPanel.add(okButton, "cell 2 3,growx");
+		mainPanel.add(viewLicenseButton, "cell 1 3");
+		mainPanel.add(view3rdLicenseButton, "flowx,cell 1 3");
+
 		okButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed (ActionEvent e) {
 				dispose();
 			}
 		});
-		mainPanel.add(new JLabel(App.APP_NAME + " " + App.APP_VERSION), "cell 0 0");
-		mainPanel.add(new JLabel("Licenses information:"), "cell 0 1");
-		mainPanel.add(new JScrollPane(licenseTextArea), "cell 0 2,grow");
-		mainPanel.add(okButton, "cell 0 3,alignx right");
+		
+		viewLicenseButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed (ActionEvent e) {
+				new ViewTextFileDialog(instance, "License", StringUtils.toString(App.getResourceAsStream("/data/LICENSE")));
+			}
+		});
+
+		view3rdLicenseButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed (ActionEvent e) {
+				new ViewTextFileDialog(instance, "3rd licenses", StringUtils.toString(App.getResourceAsStream("/data/LICENSE3RD")));
+			}
+		});
 
 		setVisible(true);
 	}
