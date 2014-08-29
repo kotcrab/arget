96f409d3f2661f91ab0dffb7c2194f53d56236a4
Paweł Pastuszak <kotcrab@gmail.com>
2014-08-30 00:15:31 +0200

    Fixed NullPointerException

--- src/pl/kotcrab/arget/gui/MainWindow.java
+++ src/pl/kotcrab/arget/gui/MainWindow.java
@@ -416,10 +416,13 @@ public class MainWindow extends JFrame implements MainWindowCallback, EventListe
 	public void connectToServer (ServerDescriptor info) {
 		if (client == null) {
 			client = new ArgetClient(info, profile, instance, sessionWindowManager);
+			
+			ArgetClient clientRef = client; //client may be changed by set to null by some event
+			
 			sessionWindowManager.setLocalSessionManager(client.getLocalSessionManager());
 
-			if (client.isSuccessfullyInitialized() == false) {
-				client.requestDisconnect();
+			if (clientRef.isSuccessfullyInitialized() == false) {
+				clientRef.requestDisconnect();
 				client = null;
 			}
 		} else
